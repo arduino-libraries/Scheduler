ce19aea65fb174589f54e6aa681cb3ac270c5e18
Testato <Testato@users.noreply.github.com>
2016-01-01 23:08:40 +0100

    Update Loop2.ino

--- examples/MultiplePWM/Loop2.ino
+++ examples/MultiplePWM/Loop2.ino
@@ -2,11 +2,7 @@
 // Task no.2
 
 void loop2 () {
- analogWrite(led1, counter1);
+ analogWrite(LED2, counter1);
  counter1 += 50;
-
-  // When multiple tasks are running, 'delay' passes control to
-  // other tasks while waiting and guarantees they get executed
-  // It is not necessary to call yield() when using delay() 
-  delay(1000);
+ delay(500);
 }
